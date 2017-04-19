95d1c0102b0fe70b8809bb0cec88cfcaa214b142
Stefan Silviu <sylviu44@gmail.com>
2017-04-19 18:48:42 +0300

    Fix route paths

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile('./view/index.html');
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.get('/login', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../views/login.html'));
 });
 
 module.exports = router;
