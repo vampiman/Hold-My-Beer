c251a37ab0bb310aa3445662f11f3b3745c5d265
Stefan Silviu <sylviu44@gmail.com>
2017-04-20 14:10:41 +0300

    Use sane routing for images

--- app.js
+++ app.js
@@ -25,11 +25,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-// images
-app.use(express.static(path.join(__dirname, 'images')));
-
-// compiled stylesheets/scripts
+// Routes
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/', index);
 
 // catch 404 and forward to error handler
