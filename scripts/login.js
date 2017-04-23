4f355e6e344f7c674874e2fd912d1d342165fd12
Stefan Silviu <sylviu44@gmail.com>
2017-04-23 18:46:34 +0300

    Start work on validation

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,15 @@
 'use strict';
+function isEmailValid(email) {
+  return /\S+@\S+\.\S+/.test(email);
+}
+
 const loginForm = $('form[action="/login"]');
 loginForm.submit(() => {
+  if (!isEmailValid($('form[action="/login"] input[name="email"]')[0].value)) {
+    // FIXME show user something
+    return false;
+  }
+  // FIXME validate password
   $.post($(loginForm).attr('action'), $(loginForm).serialize(), json => {
     location.assign('/');
   }, 'json').fail(response => {
@@ -9,3 +18,5 @@ loginForm.submit(() => {
   });
   return false;
 });
+
+// FIXME validate register form
