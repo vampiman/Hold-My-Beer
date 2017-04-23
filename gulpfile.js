a99a808aee79e9c18942f308822bfaaa9341eebf
Stefan Silviu <sylviu44@gmail.com>
2017-04-23 19:09:29 +0300

    Fix name of compiled js

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const less = require('gulp-less');
 const cleanCss = require('gulp-clean-css');
 const minifyJs = require('gulp-minify');
 const sequence = require('gulp-sequence');
+const rename = require('gulp-rename');
 
 const jsSrcGlob = './scripts/*.js';
 const cssGlob = './stylesheets/*.less';
@@ -36,6 +37,9 @@ gulp.task('build-js', () =>
   gulp.src(jsSrcGlob)
     .pipe(babel())
     .pipe(minifyJs())
+    .pipe(rename(path => {
+      path.basename = path.basename.split('-min')[0];
+    }))
     .pipe(gulp.dest('./dist/scripts/'))
 );
 
