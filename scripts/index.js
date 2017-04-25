9e926c20743162d116199acbf099126a18b3a77a
Stefan Silviu <sylviu44@gmail.com>
2017-04-25 15:32:27 +0300

    Add error on missing content

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,9 @@
 'use strict';
 if ($('#navbar')[0].dataset.loggedin === 'false') $('.modal').modal('show');
+$.get('/content/homepage', data => {
+  $('main').append(data);
+}, 'html').fail(response => {
+  console.log(response);
+  $('main')[0].dataset.error = 'true';
+  $('aside.error-no-content')[0].dataset.show = 'true';
+});
