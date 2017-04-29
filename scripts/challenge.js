e3dee69dcc98913025ccd8e5e15424df8876428e
Stefan Silviu <sylviu44@gmail.com>
2017-04-29 12:58:05 +0300

    Hide popups if no error was found

--- scripts/challenge.js
+++ scripts/challenge.js
@@ -11,6 +11,8 @@ form.submit(event => {
   } else if (title && title.length > 40) {
     isValid = false;
     ErrorPopup.create(form, 0, i18n.titleLong);
+  } else {
+    ErrorPopup.remove(form, 0);
   }
   const desc = form.find('textarea[name="desc"]').val();
   if (!desc) {
@@ -19,6 +21,8 @@ form.submit(event => {
   } else if (desc && desc.length > 250) {
     isValid = false;
     ErrorPopup.create(form, 1, i18n.descLong);
+  } else {
+    ErrorPopup.remove(form, 1);
   }
   if (!isValid) return false;
   $.post($(form).attr('action'), $(form).serialize(), json => {
