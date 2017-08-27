40531539f23bb8bb8f4c2e93cc309c681c1791dc
vampiman <robert.leu2009@yahoo.com>
2017-08-27 16:16:23 +0300

    Removed navbar script

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,3 @@
 'use strict';
 
 attachLiveContent('homepage');
-
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
